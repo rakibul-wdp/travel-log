b694cf9e076689d8db64af42dd3206b71178bb19
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-08 21:28:02 +0600

    create TravelLog functionality

--- src/models/TravelLog/TravelLog.ts
+++ src/models/TravelLog/TravelLog.ts
@@ -1,1 +1,22 @@
-export default "hello world";
+import { z } from "zod";
+
+const errors = {
+  title: "Title cannot be empty.",
+  description: "Description cannot be empty.",
+  url: "Image must be a valid URL",
+};
+
+export const TravelLog = z.object({
+  title: z.string().trim().min(1, errors.title),
+  description: z.string().trim().min(1, errors.description),
+  image: z.string().url(errors.url),
+  rating: z.coerce.number().min(0).max(10).default(0),
+  latitude: z.coerce.number().min(-90).max(90),
+  longitude: z.coerce.number().min(-180).max(180),
+  visitDate: z.coerce.date(),
+});
+
+export const TravelLogProperties = TravelLog.keyof().Enum;
+export type TravelLogProperty = keyof typeof TravelLogProperties;
+
+export type TravelLog = z.infer<typeof TravelLog>;
