56c48eeece3f2398d83a11a0baefaffbea803388
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-09 19:21:52 +0600

    create travelLogMap ui

--- src/components/TravelLogMap.tsx
+++ src/components/TravelLogMap.tsx
@@ -4,7 +4,7 @@ import L from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
 import "leaflet/dist/leaflet.css";
-import { useMap } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
 import type { TravelLogWithId } from "@/models/TravelLog/TravelLogs";
 import { useLayoutEffect } from "react";
@@ -38,3 +38,35 @@ const InitMap = ({ logs }: TravelLogMapProps) => {
   }, [map, logs]);
   return null;
 };
+
+export default function TravelLogMap({ logs }: TravelLogMapProps) {
+  if (!process.env.NEXT_PUBLIC_MAP_TILE_URL) {
+    throw new Error("Missing NEXT_PUBLIC_MAP_TILE_URL in .env.local");
+  }
+  return (
+    <MapContainer className="w-full h-full" style={{ background: "#242525" }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url={process.env.NEXT_PUBLIC_MAP_TILE_URL}
+      />
+      <InitMap logs={logs} />
+      {logs.map((log) => (
+        <Marker
+          key={log._id.toString()}
+          position={[log.latitude, log.longitude]}
+        >
+          <Popup offset={[0, -32]}>
+            <p className="text-lg font-bold">{log.title}</p>
+            <div className="flex justify-center items-center">
+              <img alt={log.title} src={log.image} className="w-96" />
+            </div>
+            <p>{log.description}</p>
+            <p className="text-sm italic">
+              {new Date(log.visitDate.toString()).toLocaleDateString()}
+            </p>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
