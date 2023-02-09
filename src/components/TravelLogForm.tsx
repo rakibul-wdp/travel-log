7fc03b74f48436c93c5dde6c99757e923ce7703e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-09 16:13:19 +0600

    create travel log form ui

--- src/components/TravelLogForm.tsx
+++ src/components/TravelLogForm.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { TravelLogProperty } from "@/models/TravelLog/TravelLog";
+import { TravelLog, TravelLogProperty } from "@/models/TravelLog/TravelLog";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 const travelLogInputs: Record<
   TravelLogProperty,
@@ -38,3 +41,74 @@ const padNum = (input: number) => input.toString().padStart(2, "0");
 const nowString = `${now.getFullYear()}-${padNum(now.getMonth() + 1)}-${padNum(
   now.getDate()
 )}}`;
+
+export default function TravelLogForm() {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TravelLog>({
+    resolver: zodResolver(TravelLog),
+    defaultValues: {
+      title: "",
+      description: "",
+      rating: 5,
+      latitude: 90,
+      longitude: 180,
+      // @ts-ignore
+      visitDate: nowString,
+    },
+  });
+
+  const onSubmit: SubmitHandler<TravelLog> = async (data) => {
+    const response = await fetch("/api/logs", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const json = await response.json();
+    router.push("/");
+  };
+
+  return (
+    <form
+      className="mx-auto max-w-md flex gap-4 flex-col my-4"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      {Object.entries(travelLogInputs).map(([name, value]) => {
+        const property = name as TravelLogProperty;
+        return (
+          <div key={name} className="form-control w-full">
+            <label className="label">
+              <span className="label-text capitalize">
+                {value.label || name}
+              </span>
+            </label>
+            {value.type === "textarea" ? (
+              <textarea
+                className={`textarea textarea-bordered w-full ${
+                  errors.description ? "textarea-error" : ""
+                }`}
+                {...register(property)}
+              />
+            ) : (
+              <input
+                type={value.type}
+                step="any"
+                className={`input input-bordered w-full ${
+                  errors[property] ? "input-error" : ""
+                }`}
+                {...register(property)}
+              />
+            )}
+            {errors[property] && <span>{errors[property]?.message}</span>}
+          </div>
+        );
+      })}
+      <button className="btn btn-success">Create</button>
+    </form>
+  );
+}
