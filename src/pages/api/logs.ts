7cbca0b7b5881af501c0005176e03d849bc86631
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-09 15:09:53 +0600

    create post get functionality in logs file

--- src/pages/api/logs.ts
+++ src/pages/api/logs.ts
@@ -1,13 +1,39 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string
-}
+import {
+  TravelLog,
+  TravelLogs,
+  TravelLogWithId,
+} from "@/models/TravelLog/TravelLogs";
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<
+    TravelLogWithId | TravelLogWithId[] | { message: string }
+  >
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  try {
+    switch (req.method) {
+      case "POST": {
+        const validatedLog = await TravelLog.parseAsync(req.body);
+        const insertResult = await TravelLogs.insertOne(validatedLog);
+        return res.status(200).json({
+          ...validatedLog,
+          _id: insertResult.insertedId,
+        });
+      }
+      case "GET": {
+        const logs = await TravelLogs.find().toArray();
+        return res.status(200).json(logs);
+      }
+      default: {
+        return res.status(405).json({ message: "Not Supported" });
+      }
+    }
+  } catch (e) {
+    const error = e as Error;
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
 }
